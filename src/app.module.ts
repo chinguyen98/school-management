1a4af79109773b9f8e7009bdadfc25e61a4e0457
chinguyen98 <dacchi6761@gmail.com>
2020-05-12 22:57:30 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -13,12 +15,14 @@ import { Lesson } from './lesson/lesson.entity';
       useUnifiedTopology: true,
       entities: [
         Lesson,
+        Student,
       ],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     LessonModule,
+    StudentModule,
   ],
 })
 export class AppModule { }
